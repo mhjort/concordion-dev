0fab3b107f0c35a64647b463079a9970e9cc9bd8
peterson3 <peterson3@430547ba-244a-0410-acf3-eb946b82c357>
2009-03-05 18:34:40 +0000

    Improvements to the tests for the concordion:run command to make the documentation clearer and to test more of the logic.

--- src/test/java/spec/concordion/command/run/RunTest.java
+++ src/test/java/spec/concordion/command/run/RunTest.java
@@ -1,18 +1,7 @@
 package spec.concordion.command.run;
 
-import org.concordion.api.Result;
 import org.concordion.integration.junit3.ConcordionTestCase;
 
-import test.concordion.TestRig;
-
 public class RunTest extends ConcordionTestCase{
-	public String successOrFailure(String fragment, String hardCodedTestResult, String evaluationResult) {
-		
-		System.setProperty("concordion.runner.concordion", RunTestRunner.class.getName());
-		RunTestRunner.result = Result.valueOf(hardCodedTestResult);
-        return new TestRig()
-            .withStubbedEvaluationResult(evaluationResult)
-            .processFragment(fragment)
-            .successOrFailureInWords();
-    }
+
 }
